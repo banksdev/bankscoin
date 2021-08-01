0565715520a2b2b32d327ed712a4e9be78b2aabd
Alexander Banks <banksdev@Alexanders-MacBook-Pro.local>
2021-08-01 12:23:48 +0200

    moved tests

--- tests/block.test.ts
+++
@@ -1,8 +0,0 @@
-import { Block } from '../src/block';
-
-test('nonces are random', () => {
-    const a = new Block('',null);
-    const b = new Block('', null);
-
-    expect(a.nonce).not.toBe(b.nonce);
-});
--- tests/transaction.test.ts
+++
@@ -1,8 +0,0 @@
-import { Transaction } from '../src/transaction';
-
-test('transaction correctly constructs', () => {
-    const transaction = new Transaction(100, "Banks", "Kosta");
-    expect(transaction.amount).toBe(100);
-    expect(transaction.payer).toBe("Banks");
-    expect(transaction.payee).toBe("Kosta");
-});
